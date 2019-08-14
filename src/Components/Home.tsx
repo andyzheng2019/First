bfcdd4c3d21b5641199bbe6a9a86efbd4c183edc
bindezheng <bindezheng@awh.com.au>
2019-08-14 18:23:16 +1000

    Add router

---
+++ src/Components/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Home extends React.PureComponent{
+
+  
+    
+    render(){
+        return <h1>
+            Welcome to home, click <a href='/recipes'>here</a> to access all the recipes
+        </h1>
+    }
+
+}
+
+
---
+++ src/Components/RecipesContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+  
+export default class RecipesContainer extends React.PureComponent{
+
+    render(){
+        return <h1>Welcome to recipes, click <a href='/'>here</a> to go back home page</h1>
+    }
+
+}
