d41be677a49c82c6a6b6ce79fb465e46435377e5
bindezheng <bindezheng@awh.com.au>
2019-08-14 19:46:12 +1000

    process data in redux

---
+++ src/CommonType/RecipeData.ts
@@ -0,0 +1,16 @@
+export interface IRecipeData  {
+    key: string;
+    title: string;
+    small1x: string;
+    small2x: string;
+    small3x: string;
+    medium1x: string;
+    medium2x: string;
+    medium3x: string;
+    mobile1x: string;
+    mobile2x: string;
+    mobile3x: string;
+    desktop1x: string;
+    desktop2x: string;
+    desktop3x: string;
+  }
--- src/Components/RecipesContainer.tsx
+++ src/Components/RecipesContainer.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import {Container,Row} from "react-bootstrap"
 
 import getAllRecipeData from '../redux/actions/actionCreators'
 import { connect } from "react-redux";
+import { IRecipeData } from "../CommonType/RecipeData";
+
+
+type ContainerProps = {
+    data: IRecipeData[],
+    getAllRecipeData: any,
+  }
   
-class RecipesContainer extends React.PureComponent{
-        
+class RecipesContainer extends React.PureComponent<ContainerProps>{
+
+    componentDidMount(){
+        this.props.getAllRecipeData()
+    }
+    
     render(){
-        return  <h1>Welcome to recipes, click <a href='/'>here</a> to go back home page</h1>
+        return <h1>Welcome to recipes, click <a href='/'>here</a> to go back home page</h1>
     }
 
 }
@@ -23,7 +35,28 @@ const mapStateToProps = (state: { allRecipesReducer: any; }) => {
 
             let jsonObj = JSON.parse(json)
 
-            let data: any[] | never[] = []
+            let data = []
+            let arr = jsonObj.contents;
+            let len = jsonObj.contents.length
+            for(let i = 0;i<len;i++){
+                let obj: IRecipeData = {
+                key: arr[i].contentId,
+                title: arr[i].title,    
+                small1x: arr[i].imageList.landscape32small1x.url,
+                small2x: arr[i].imageList.landscape32small2x.url,
+                small3x: arr[i].imageList.landscape32small3x.url,
+                medium1x : arr[i].imageList.landscape32medium1x.url,
+                medium2x : arr[i].imageList.landscape32medium2x.url,
+                medium3x : arr[i].imageList.landscape32medium3x.url,
+                mobile1x : arr[i].imageList.landscapemobile1x.url,
+                mobile2x : arr[i].imageList.landscapemobile2x.url,
+                mobile3x : arr[i].imageList.landscapemobile3x.url,
+                desktop1x: arr[i].imageList.landscapewidedesktop1x.url,
+                desktop2x: arr[i].imageList.landscapewidedesktop2x.url,
+                desktop3x: arr[i].imageList.landscapewidedesktop3x.url,
+                };
+            data.push(obj)
+            }
             dispatch(getAllRecipeData(data))
         })
       };
